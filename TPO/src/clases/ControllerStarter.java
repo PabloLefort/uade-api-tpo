ae24dd209f636e8023cff6b5819e130fc93e04ac
opelss <gmartinquesada@gmail.com>
2018-10-10 21:21:01 -0300

    Create ControllerStarter.java

---
+++ TPO/src/clases/ControllerStarter.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class ControllerStarter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SistemaControlador controller = null;
+		controller = new SistemaControlador ();	
+		controller.start;
+	}
+
+}
