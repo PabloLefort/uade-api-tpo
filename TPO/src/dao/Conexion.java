6de650e85e92a130a32fd8e126b0defb54be6640
opelss <gmartinquesada@gmail.com>
2018-10-18 17:07:01 -0300

    Create Conexion.java

---
+++ TPO/src/dao/Conexion.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.sql.Connection;
+
+public class Conexion {
+	
+	private Connection conexion = null;
+	private Boolean estado = Boolean.FALSE;
+	private Integer posicion = null;
+	
+	public Conexion() {
+	}
+	
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
+
+	public Boolean getEstatdo() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estato) {
+		this.estado = estato;
+	}
+
+	public Integer getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Integer posicion) {
+		this.posicion = posicion;
+	}
+
+}
